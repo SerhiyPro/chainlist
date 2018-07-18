fe54d2b5a8a403e19895a49b40e4a20f511e2e8b
Serhiy Prots <prots.sergiy7@gmail.com>
2018-07-18 22:43:18 +0300

    Smart contract deactivation

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -12,6 +12,7 @@ contract ChainList {
     }
 
     //state variables
+    address owner;
     mapping (uint => Article) public articles;
     uint articleCounter;
 
@@ -30,6 +31,19 @@ contract ChainList {
         string _name,
         uint256 _price
     );
+
+    //constuctor
+    function ChainList() public{
+        owner = msg.sender;
+    }
+
+    //deactivate the contract
+    function kill() public {
+        //only allow the contract owner
+        require(msg.sender == owner);
+
+        selfdestruct(owner);
+    }
 
     //sell an article
     function sellArticle(string _name, string _description, uint256 _price) public {
